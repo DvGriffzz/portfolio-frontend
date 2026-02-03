3235c425eecccd874f342d03ba019bd653213c72
Diogo Vieira <diogovieira3@hotmail.com>
2026-02-03 18:29:30 +0000

    fixed the production environment - ready for deployment

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://portfolio-backend-production-db1c.up.railway.app/api'
+    apiUrl: 'https://portfolio-backend-production-db1c.up.railway.app/api/'
 };
